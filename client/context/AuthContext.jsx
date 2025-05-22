ee0e5fd112d42f9b69b36e41a5e834728246de1f
techzoommedia <165561362+techzoommedia@users.noreply.github.com>
2025-05-22 10:24:48 +0530

    Update AuthContext.jsx

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -77,6 +77,7 @@ export const AuthProvider = ({ children }) => {
       query: {
         userId: userData._id,
       },
+       transports: ['websocket'],
     });
     newSocket.connect();
     setSocket(newSocket);
